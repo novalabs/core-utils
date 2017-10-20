10a5c91d0e36511333250304ad14ad2269145661
Davide Rizzi <d.rizzi@novalabs.io>
2017-10-20 16:18:45 +0200

    Added Accumulator

---
+++ include/core/utils/Accumulator.hpp
@@ -0,0 +1,46 @@
+/* COPYRIGHT (c) 2016-2017 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
+#pragma once
+
+#include <core/utils/namespace.hpp>
+
+NAMESPACE_CORE_UTILS_BEGIN
+
+template <typename T>
+class Accumulator {
+    public:
+        Accumulator() {reset();};
+
+        void reset() {
+            _accumulator = T();
+            _counter = 0;
+        }
+
+        inline T value() {
+            return _accumulator;
+        }
+
+        inline std::size_t counter() {
+            return _counter;
+        }
+
+        template<typename TT>
+        void add(TT x);
+    private:
+        T _accumulator;
+        std::size_t _counter;
+
+};
+
+template<typename T>
+template<typename TT>
+void Accumulator<T>::add(TT x) {
+    _accumulator += x;
+    _counter++;
+}
+
+NAMESPACE_CORE_UTILS_END
